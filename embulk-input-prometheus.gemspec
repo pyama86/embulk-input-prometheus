cec5e0790e7ea2deffe413dc49580c90606899ac
pyama86 <pyama@pepabo.com>
2020-04-23 17:02:21 +0900

    it work perfectly

--- embulk-input-prometheus.gemspec
+++ embulk-input-prometheus.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
   spec.files         = `git ls-files`.split("\n") + Dir["classpath/*.jar"]
   spec.test_files    = spec.files.grep(%r{^(test|spec)/})
   spec.require_paths = ["lib"]
-
+  spec.add_dependency 'prometheus-api-client'
   #spec.add_dependency 'YOUR_GEM_DEPENDENCY', ['~> YOUR_GEM_DEPENDENCY_VERSION']
   spec.add_development_dependency 'embulk', ['>= 0.8.39']
   spec.add_development_dependency 'bundler', ['>= 1.10.6']
--- lib/embulk/input/prometheus.rb
+++ lib/embulk/input/prometheus.rb
@@ -1,20 +1,29 @@
+require 'prometheus/api_client'
+require 'openssl'
+require 'json'
+require 'pp'
+
 module Embulk
   module Input
 
     class Prometheus < InputPlugin
       Plugin.register_input("prometheus", self)
-
       def self.transaction(config, &control)
-        # configuration code:
         task = {
-          "option1" => config.param("option1", :integer),                     # integer, required
-          "option2" => config.param("option2", :string, default: "myvalue"),  # string, optional
-          "option3" => config.param("option3", :string, default: nil),        # string, optional
+          "url" => config.param("url", :string, default: 'http://localhost:9090/api/v1/'),
+          "query" => config.param("query", :string),
+          "since" => config.param("since", :integer),
+          "step" => config.param("step", :integer),
+          "element_key" => config.param("element_key", :string, default: 'instance'),
+          "tls" => config.param("tls", :hash, default: nil),
+          "token" => config.param("token", :string,  default: nil),
+          "timeout" => config.param("timeout", :interger, default: 60),
+          "open_timeout" => config.param("open_timeout", :interger, default: 10),
         }
 
         columns = [
-          Column.new(0, "example", :string),
-          Column.new(1, "column", :long),
+          Column.new(0, "name", :string),
+          Column.new(1, "time", :double),
           Column.new(2, "value", :double),
         ]
 
@@ -28,32 +37,47 @@ module Embulk
         return next_config_diff
       end
 
-      # TODO
-      # def self.guess(config)
-      #   sample_records = [
-      #     {"example"=>"a", "column"=>1, "value"=>0.1},
-      #     {"example"=>"a", "column"=>2, "value"=>0.2},
-      #   ]
-      #   columns = Guess::SchemaGuess.from_hash_records(sample_records)
-      #   return {"columns" => columns}
-      # end
-
-      def init
-        # initialization code:
-        @option1 = task["option1"]
-        @option2 = task["option2"]
-        @option3 = task["option3"]
-      end
-
       def run
-        page_builder.add(["example-value", 1, 0.1])
-        page_builder.add(["example-value", 2, 0.2])
-        page_builder.finish
+        params = {
+          url: task['url'],
+          options: {},
+          ssl: {},
+          credentials: {},
+        }
 
+        params[:ssl][:client_cert] = OpenSSL::X509::Certificate.new(File.read(task["tls"]["cert_path"])) if task["tls"]["cert_path"]
+        params[:ssl][:client_key] = OpenSSL::PKey::RSA.new(File.read(task["tls"]["key_path"])) if task["tls"]["key_path"]
+        params[:ssl][:ca_file] = task["tls"]["ca_path"] if task["tls"]["ca_path"]
+
+        params[:credentials][:token] = task["token"] if task["token"]
+
+        %w(
+          open_timeout
+          timeout
+        ).each do |n|
+          params[:options][n.to_sym] =  task[n] if task[n]
+        end
+
+        prometheus = ::Prometheus::ApiClient.client(params)
+        result = JSON.parse(prometheus.get(
+          'query_range',
+          query: task['query'],
+          start: (Time.now - task['since']).strftime("%Y-%m-%dT%H:%M:%S.%LZ"),
+          end:   Time.now.strftime("%Y-%m-%dT%H:%M:%S.%LZ"),
+          step:  "#{task['step']}s",
+        ).body)
+
+        if result['status'] == 'success'
+          result['data']['result'].each do |r|
+            r["values"].each do |v|
+              page_builder.add([r["metric"][task["element_key"]], v[0], v[1]])
+            end
+          end
+        end
+        page_builder.finish
         task_report = {}
         return task_report
       end
     end
-
   end
 end
